b70583f8b8e55279feb7b4318430fc562f5eb19c
giuem <giuemcom@gmail.com>
2018-08-26 19:11:29 +0800

    fix: typography style update

--- src/script/icon.js
+++ src/script/icon.js
@@ -1,13 +1,13 @@
 import { dom, library } from "@fortawesome/fontawesome-svg-core";
 import {
-  faCalendarAlt,
+  faCalendar,
   faArrowAltCircleLeft,
   faArrowAltCircleRight
 } from "@fortawesome/free-regular-svg-icons";
 import { faTag, faFolder, faHeart } from "@fortawesome/free-solid-svg-icons";
 
 library.add(
-  faCalendarAlt,
+  faCalendar,
   faArrowAltCircleLeft,
   faArrowAltCircleRight,
   faTag,
