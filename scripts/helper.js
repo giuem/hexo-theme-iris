f4e96914875fc73c62f3bdcccb4faab409067c45
giuem <giuemcom@gmail.com>
2019-02-15 00:03:02 +0800

    fix: catch error when manifest file no found

--- scripts/helper.js
+++ scripts/helper.js
@@ -1,8 +1,11 @@
 hexo.extend.helper.register("get_rev_name", function(name) {
-  const manifest_PATH = "../source/dist/rev-manifest.json";
-  const manifest = require(manifest_PATH);
-  // delete manifest cache
-  delete require.cache[require.resolve(manifest_PATH)];
+  let manifest = {};
+  try {
+    const manifest_PATH = "../source/dist/rev-manifest.json";
+    manifest = require(manifest_PATH);
+    // delete manifest cache
+    delete require.cache[require.resolve(manifest_PATH)];
+  } catch (error) {}
 
   return manifest[name] ? manifest[name] : name;
 });
