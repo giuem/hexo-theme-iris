c41b9b7278ec75bd1bc9b6d5b267ac1681a997c9
giuem <giuemcom@gmail.com>
2019-09-09 22:31:13 +0800

    feat: toc (#8)

--- src/script/index.js
+++ src/script/index.js
@@ -3,12 +3,14 @@ import IconInit from "./icon";
 import ZoomInit from "./zoom";
 import NavInit from "./nav";
 import QuickLinkInit from "./quicklink";
+import TocInit from "./toc";
 
 function init() {
   IconInit();
   NavInit();
   ZoomInit();
   QuickLinkInit();
+  TocInit();
 }
 
 if (document.readyState === "loading") {
---
+++ src/script/toc.js
@@ -0,0 +1,46 @@
+export default function() {
+  // only on post page
+  if (!document.querySelector(".post")) return;
+
+  const headingTocLinks = document.querySelectorAll(".post-toc a");
+  let lastVisibleHeadingTocLink = null;
+
+  const intersectionObserver = new IntersectionObserver(
+    entries => {
+      console.log(entries)
+      entries.forEach(entry => {
+        const href = entry.target.getAttribute("href");
+        const headingTocLink = document.querySelector(
+          `.post-toc a[href="${href}"]`
+        );
+        if (!headingTocLink) return;
+
+        if (entry.intersectionRatio > 0) {
+          headingTocLink.classList.add("is-visible");
+          lastVisibleHeadingTocLink = headingTocLink;
+        } else {
+          headingTocLink.classList.remove("is-visible");
+        }
+      });
+
+      // remove all active link
+      headingTocLinks.forEach(el => el.classList.remove("active"));
+
+      // find first visible link or last visible link
+      const [
+        activeHeadingTocLink = lastVisibleHeadingTocLink
+      ] = document.querySelectorAll(".is-visible");
+      if (activeHeadingTocLink) {
+        activeHeadingTocLink.classList.add("active");
+      }
+    },
+    {
+      rootMargin: "0px",
+      threshold: 1.0
+    }
+  );
+
+  document
+    .querySelectorAll(".post .headerlink")
+    .forEach(el => intersectionObserver.observe(el));
+}
